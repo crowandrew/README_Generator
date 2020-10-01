fb147c0fcd77229215e75e156dd5e556a04d070b
Andrew Crow <andrew@crow.me>
2020-10-01 16:53:56 -0700

    uncommented lines in index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const {questions} = require("./utils/questions.js");
 async function init(){
     const answers = await inquirer.prompt(questions);
     console.log(answers);
-    // const userReadME = generateMarkdown(answers);
-    // fs.promises.writeFile("README.md",userReadME);
+    const userReadME = generateMarkdown(answers);
+    fs.promises.writeFile("README.md",userReadME);
 }
  
 // function call to initialize program
