62b549ffcee2f0680f2d5da25056f6a071973e56
Andrew Crow <andrew@crow.me>
2020-10-01 14:29:52 -0700

    added questions.js and wrote initial index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const {questions} = require("./utils/questions.js");
 
+// function to initialize program
+async function init(){
+    const userAnswers = await inquirer.prompt(questions);
+    const userReadME = generateMarkdown(userAnswers);
+    fs.promises.writeFile("README.md",userReadME);
+}
+ 
+// function call to initialize program
+init();
